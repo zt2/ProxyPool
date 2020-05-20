3861be7ef534eb464f2523314dc18452734555d3
zt2 <ztz5651483@gmail.com>
2020-05-20 15:22:54 +0800

    adding test

--- test/proxy_pool_test.rb
+++ test/proxy_pool_test.rb
@@ -7,7 +7,30 @@ class ProxyPoolTest < Minitest::Test
     refute_nil ::ProxyPool::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_proxy_get
+    refute_nil ::ProxyPool.get_anonymous_proxy
+    refute_nil ::ProxyPool.get_transparent_proxy
+  end
+
+  def test_get_anonymous_proxy
+    proxy = ::ProxyPool.get_anonymous_proxy
+    assert_equal 'high_anonymous', proxy['anonymity']
+  end
+
+  def test_get_transparent_proxy
+    proxy = ::ProxyPool.get_transparent_proxy
+    refute_equal 'high_anonymous', proxy['anonymity']
+  end
+
+  def test_remove_proxy_from_pool
+    proxy = ::ProxyPool.get_transparent_proxy
+    assert_equal true, ::ProxyPool::Dealer.instance.transparent_pools.include?(proxy)
+    ::ProxyPool.remove(proxy)
+    assert_equal false, ::ProxyPool::Dealer.instance.transparent_pools.include?(proxy)
+
+    proxy = ::ProxyPool.get_anonymous_proxy
+    assert_equal true, ::ProxyPool::Dealer.instance.anonymous_pools.include?(proxy)
+    ::ProxyPool.remove(proxy)
+    assert_equal false, ::ProxyPool::Dealer.instance.anonymous_pools.include?(proxy)
   end
 end
